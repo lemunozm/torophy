c9686e0af8518f889ff9d9f394a7901365d947b3
lemunozm <lemunozm@gmail.com>
2020-06-17 16:58:52 +0200

    Added particle front example

--- examples/particles_front.rs
+++ examples/particles_front.rs
@@ -8,12 +8,11 @@ fn main() {
     let mut space = Space::new(800, 600);
 
     let angle_dist = Uniform::new(-std::f32::consts::PI, std::f32::consts::PI);
-    let speed_dist = Uniform::new(5.0, 100.0);
     let mut rng = StdRng::seed_from_u64(0);
 
     for _ in 0..1000 {
         let mut body = Body::new(space.bounds().dimension() / 2.0);
-        body.set_velocity(Vec2::from_angle(rng.sample(angle_dist)) * rng.sample(speed_dist));
+        body.set_velocity(Vec2::from_angle(rng.sample(angle_dist)) * 100.0);
         space.add(body);
     }
 
